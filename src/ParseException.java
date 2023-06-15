85e7ea2a4f50b741de0c957c42ba41763c543be5
Татьяна <1975tanya@bk.ru>
2023-06-16 01:09:37 +0400

    Initial commit

---
+++ src/ParseException.java
@@ -0,0 +1,8 @@
+
+public class ParseException extends Exception {
+
+    public ParseException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/UserDataFileWriter.java
@@ -0,0 +1,7 @@
+import java.io.IOException;
+
+public interface UserDataFileWriter {
+
+    void writeUserData(UserData userData) throws IOException;
+
+}
